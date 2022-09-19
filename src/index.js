361427dc51966e1aaa563cac77ec6360ac857fc9
Ben McShane <benjaminmartinmc@gmail.com>
2022-09-19 13:13:13 -0600

    allow access

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ import { BrowserRouter } from "react-router-dom";
 
 const port = process.env.PORT || 4000;
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+var corsOptions = {
+    origin: 'https://toad-test-abl.herokuapp.com/leaders',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
 root.render(
     <BrowserRouter>
     <App />
